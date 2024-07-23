b76cc197fb0bc37454425a1ca91ef7a834e3c0cf
Rodion <rodion.groshev@gmail.com>
2024-07-23 21:08:46 +0200

    HW_01 done

---
+++ src/js/2-form.js
@@ -0,0 +1,46 @@
+const feedbackForm = document.querySelector('.feedback-form');
+const data = {};
+
+const fillFormFields = form => {
+  const formDataFromLS = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  );
+
+  if (formDataFromLS === null) {
+    return;
+  }
+
+  for (const key in formDataFromLS) {
+    if (formDataFromLS.hasOwnProperty(key)) {
+      form.elements[key].value = formDataFromLS[key];
+      data[key] = formDataFromLS[key];
+    }
+  }
+};
+
+fillFormFields(feedbackForm);
+
+const onFormFieldChange = event => {
+  const fieldName = event.target.name;
+  const fieldValue = event.target.value.trim();
+
+  data[fieldName] = fieldValue;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+};
+
+const onFeedbackFormSubmit = event => {
+  event.preventDefault();
+  if (Object.keys(data).length !== 2) {
+    alert('Fill all field');
+
+    return;
+  }
+
+  console.log(data);
+  event.target.reset();
+  localStorage.removeItem('feedback-form-state');
+};
+
+feedbackForm.addEventListener('change', onFormFieldChange);
+feedbackForm.addEventListener('submit', onFeedbackFormSubmit);
